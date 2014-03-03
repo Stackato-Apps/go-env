22942aa8c683eef5e3beaedd30813e0cccb3b753
Sridhar Ratnakumar <sridharr@activestate.com>
2014-03-03 14:55:22 -0800

    make /crash crash the web process
    handy for debugging or testing Stackato.

--- env.go
+++ env.go
@@ -11,6 +11,9 @@ import (
 func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%+v\n", req)
 	fmt.Fprintln(w, strings.Join(os.Environ(), "\n"))
+	if req.URL.Path == "/crash" {
+		os.Exit(1)
+	}
 }
 
 func main() {
