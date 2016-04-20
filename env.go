6ee766695f0ed85a475d3a42bf395cd0ee8c5887
Mark Yen <mook@hpe.com>
2016-04-20 11:13:27 -0700

    Add endpoint to print out headers, and adjust how the endpoints work.

--- env.go
+++ env.go
@@ -11,13 +11,25 @@ import (
 func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%+v\n", req)
 	fmt.Fprintln(w, strings.Join(os.Environ(), "\n"))
-	if req.URL.Path == "/crash" {
-		os.Exit(1)
+}
+
+func crashHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Crashing...")
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
 	}
+	os.Exit(1)
+}
+
+// headerHandler prints out the active headers in the request
+func headersHandler(w http.ResponseWriter, req *http.Request) {
+	req.Header.Write(w)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/crash", crashHandler)
+	http.HandleFunc("/headers", headersHandler)
 	addr := ":" + os.Getenv("PORT")
 	fmt.Printf("Listening on %v\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
